f138b426fc8f295e4f0851680afee7a9490fbe7b
SongHia <song.hia@gmail.com>
2015-11-04 18:52:08 -0500

    added view

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,38 @@
 // CUSTOM JS FILE //
+
+function init() {
+  renderRecord();
+}
+
+function renderRecord(){
+	jQuery.ajax({
+		url : '/api/get',
+		dataType : 'json',
+		success : function(response) {
+			console.log(response);
+
+			var record = response.record;
+
+			for(var i=0;i<record.length;i++){
+				var htmlToAdd = '<div class="col-md-4">'+
+					// '<img src='+record[i].imageUrl+' width="100">'+
+					'<h1>'+record[i].til+'</h1>'+
+					'<ul>'+
+						'<li>Context: '+record[i].context+'</li>'+
+						'<li>Tags: '+record[i].tags+'</li>'+
+						'<li>Best Part Of The Day: '+record[i].bestPartDay+'</li>'+
+						'<li>Best Link: '+record[i].pageURL+'</li>'+
+					'</ul>'+
+					'<a href="/edit/'+record[i]._id+'">Edit Record</a>'+
+				'</div>';
+			
+				jQuery("#record-holder").append(htmlToAdd);
+			}
+
+
+
+		}
+	})	
+}
+
+window.addEventListener('load', init())
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
+
+// GET routes
 // http://localhost:3000/api/get
-// (GET route)
 // http://localhost:3000/api/get/:id
-// (GET route - copy and paste the id in place of :id... you can see the id in the response of http://localhost:3000/api/get)
 // http://localhost:3000/api/delete/:id
-// (GET route - copy and paste the id in place of :id... you can see the id in the response of http://localhost:3000/api/get)
-// http://localhost:3000/api/update/:id
-// (POST route - use POSTMAN)
+// http://localhost:3000/add-til
+// http://localhost:3000/directory
+
+// (POST routes - use POSTMAN)
 // http://localhost:3000/api/create
-// (POST route - use POSTMAN)
+// http://localhost:3000/api/update/:id
+
+//https://github.com/sslover/designing-for-data-personalization/blob/master/week8/mongoose-cheatsheet.md
 
 
 
@@ -62,7 +65,7 @@ router.post('/api/create', function(req, res){
   console.log(req.body);
   
   var recordObj = {
-    tilID: req.body.tilID,
+    // tilID: req.body.tilID,
     til: req.body.til,
     context: req.body.context,
     tags: req.body.tags.split(','),
@@ -117,6 +120,7 @@ router.get('/api/get', function(req,res){
 
 })
 
+//get id
 router.get('/api/get/:id', function(req, res){
 
   var requestedId = req.param('id');
@@ -139,6 +143,110 @@ router.get('/api/get/:id', function(req, res){
     return res.json(jsonData);
   
   })
+})
+
+/**
+ * GET '/api/delete/:id'
+ * Receives a GET request specifying the animal to delete
+ * @param  {String} req.param('id'). The animalId
+ * @return {Object} JSON
+ */
+
+
+router.get('/api/delete/:id', function(req, res){
+
+  var requestedId = req.param('id');
+
+  // Mongoose method to remove, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
+  Record.findByIdAndRemove(requestedId,function(err, data){
+    if(err || data == null){
+      var error = {status:'ERROR', message: 'Could not find that record to delete'};
+      return res.json(error);
+    }
+
+    // otherwise, respond back with success
+    var jsonData = {
+      status: 'OK',
+      message: 'Successfully deleted id ' + requestedId
+    }
+
+    res.json(jsonData);
+
+  })
+
+})
+
+
+// /**
+//  * POST '/api/update/:id'
+//  * Receives a POST request with data of the animal to update, updates db, responds back
+//  * @param  {String} req.param('id'). The animalId to update
+//  * @param  {Object} req. An object containing the different attributes of the Animal
+//  * @return {Object} JSON
+//  */
+
+router.post('/api/update/:id', function(req, res){
+
+   var requestedId = req.param('id');
+
+   var dataToUpdate = {}; // a blank object of data to update
+
+    // pull out the information from the req.body and add it to the object to update
+    var til, context, tags, bestPartDay, pageURL; 
+
+    // we only want to update any field if it actually is contained within the req.body
+    // otherwise, leave it alone.
+    if(req.body.til) {
+      til = req.body.til;
+      // add to object that holds updated data
+      dataToUpdate['til'] = til;
+    }
+    if(req.body.context) {
+      context = req.body.age;
+      // add to object that holds updated data
+      dataToUpdate['context'] = context;
+    }
+    var tags = []; // blank array to hold tags
+    if(req.body.tags){
+      tags = req.body.tags.split(","); // split string into array
+      // add to object that holds updated data
+      dataToUpdate['tags'] = tags;
+    }
+    if(req.body.bestPartDay) {
+    bestPartDay = req.body.bestPartDay;
+    // add to object that holds updated data
+    dataToUpdate['bestPartDay'] = bestPartDay;
+    }
+    if(req.body.pageURL) {
+      pageURL = req.body.pageURL;
+      // add to object that holds updated data
+      dataToUpdate['pageURL'] = pageURL;
+    }    
+
+    console.log('the data to update is ' + JSON.stringify(dataToUpdate));
+
+    // now, update that animal
+    // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
+    Record.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
+      // if err saving, respond back with error
+      if (err){
+        var error = {status:'ERROR', message: 'Error updating record'};
+        return res.json(error);
+      }
+
+      console.log('Updated the record!');
+      console.log(data);
+
+      // now return the json data of the new person
+      var jsonData = {
+        status: 'OK',
+        record: data
+      }
+
+      return res.json(jsonData);
+
+    })
+
 })
 
 module.exports = router;
